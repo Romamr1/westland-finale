f8d077c20d751d0e668d840c7cc0684cccaf8770
Markus116 <yuriy.gamza@gmail.com>
2017-06-07 17:39:57 +0300

    fix mark up in employer component

--- src/app/components/employerComponent/employerHomeComponent/employerHome.component.ts
+++ src/app/components/employerComponent/employerHomeComponent/employerHome.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'employer-home-app',
-    templateUrl: './employerHome.component.html',  
-   
+    templateUrl: './employerHome.component.html', 
+    styleUrls: ['./employerHome.component.css'],   
 })
 export class EmployerHomeComponent {  
   
